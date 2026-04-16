ed9c25bbdc533f2235022542f1d51c35bcc286d3
Laurenti Loris <lloris19@outlook.fr>
2026-04-16 15:35:58 +0200

    update

--- src/modules/auth/auth-handler.ts
+++ src/modules/auth/auth-handler.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express"
-import { LoginValidator } from "../user/user-validator"
-import { generateValidationErrorMessage } from "../../utils/validators"
-import { AuthUsecase } from "./auth-usecase"
-import { AppDataSource } from "../../database/database"
-import { User } from "../../database/entities/user"
-import { Token } from "../../database/entities/token"
+// import { LoginValidator } from "../user/user-validator"
+// import { generateValidationErrorMessage } from "../../utils/validators"
+// import { AuthUsecase } from "./auth-usecase"
+// import { AppDataSource } from "../../database/database"
+// import { User } from "../../database/entities/user"
+// import { Token } from "../../database/entities/token"
 
 export const Login = async (req: Request, res: Response) => {
     console.log(req.body)
+    res.status(200).json({message: "Authentification OK"})
 }
